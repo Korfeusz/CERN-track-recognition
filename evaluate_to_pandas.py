dbeb4b37efe51ab728800fe4d5d85f287789434f
Korfeusz <brockhuis.leo@gmail.com>
2018-01-22 00:19:44 +0100

    add modified evaluate to pandas created by Rowina

---
+++ evaluate_to_pandas.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def read_data(filepath='dsdt.csv'):
+    cols1 = ['has_MCParticle', 'is_downstream_reconstructible', 'has_MCParticle_not_electron', 'is_downstream_reconstructible_not_electron', 'is_true_seed', 'seed_chi2PerDoF', 'seed_mva_value', 'seed_nbIT', 'seed_nLayers', 'seed_nLHCbIDs', 'seed_p', 'seed_pt', 'seed_tx', 'seed_ty', 'seed_x', 'seed_y']
+    result = pd.read_csv(filepath, names=cols1, index_col=False)
+    result = result[['seed_chi2PerDoF', 'seed_p', 'seed_pt', 'seed_nLHCbIDs' , 'seed_nbIT'  , 'seed_nLayers', 'seed_x' , 'seed_y' , 'seed_tx', 'seed_ty', 'is_downstream_reconstructible']]
+    return result
