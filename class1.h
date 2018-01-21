ad86c253af61db289664ebc08ecddf240ffdb058
Zuzanna Zak <5zak@taurus.fis.agh.edu.pl>
2018-01-21 20:00:17 +0100

    cpp file

---
+++ class1.h
@@ -0,0 +1,190 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Sun Jan 14 20:07:13 2018 by ROOT version 5.34/36
+// from TTree DownstreamSeedDebugTuple/DownstreamSeedDebugTuple
+// found on file: Brunel_BdJPsiKs_Magu_30k.root
+//////////////////////////////////////////////////////////
+
+#ifndef class1_h
+#define class1_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TRandom3.h>
+#include <TNtuple.h>
+#include <TH1F.h>
+#include <TLegend.h>
+#include <TF1.h>
+#include <TTree.h>
+#include <TH1D.h>
+#include <TLorentzVector.h>s
+#include<TDirectoryFile.h>
+
+// Header file for the classes stored in the TTree if any.
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+class class1 {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+   // Declaration of leaf types
+   Bool_t          has_MCParticle;
+   Bool_t          is_downstream_reconstructible;
+   Bool_t          has_MCParticle_not_electron;
+   Bool_t          is_downstream_reconstructible_not_electron;
+   Bool_t          is_true_seed;
+   Double_t        seed_chi2PerDoF;
+   Double_t        seed_p;
+   Double_t        seed_pt;
+   UInt_t          seed_nLHCbIDs;
+   UInt_t          seed_nbIT;
+   UInt_t          seed_nLayers;
+   Double_t        seed_x;
+   Double_t        seed_y;
+   Double_t        seed_tx;
+   Double_t        seed_ty;
+   Double_t        seed_mva_value;
+
+   // List of branches
+   TBranch        *b_has_MCParticle;   //!
+   TBranch        *b_is_downstream_reconstructible;   //!
+   TBranch        *b_has_MCParticle_not_electron;   //!
+   TBranch        *b_is_downstream_reconstructible_not_electron;   //!
+   TBranch        *b_is_true_seed;   //!
+   TBranch        *b_seed_chi2PerDoF;   //!
+   TBranch        *b_seed_p;   //!
+   TBranch        *b_seed_pt;   //!
+   TBranch        *b_seed_nLHCbIDs;   //!
+   TBranch        *b_seed_nbIT;   //!
+   TBranch        *b_seed_nLayers;   //!
+   TBranch        *b_seed_x;   //!
+   TBranch        *b_seed_y;   //!
+   TBranch        *b_seed_tx;   //!
+   TBranch        *b_seed_ty;   //!
+   TBranch        *b_seed_mva_value;   //!
+
+   class1(TTree *tree=0);
+   virtual ~class1();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef class1_cxx
+class1::class1(TTree *tree) : fChain(0) 
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("Brunel_BdJPsiKs_Magu_30k.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("Brunel_BdJPsiKs_Magu_30k.root");
+      }
+      TDirectory * dir = (TDirectory*)f->Get("Brunel_BdJPsiKs_Magu_30k.root:/ToolSvc.PatDebugTTTruthTool");
+      dir->GetObject("DownstreamSeedDebugTuple",tree);
+
+   }
+   Init(tree);
+}
+
+class1::~class1()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t class1::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t class1::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void class1::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("has_MCParticle", &has_MCParticle, &b_has_MCParticle);
+   fChain->SetBranchAddress("is_downstream_reconstructible", &is_downstream_reconstructible, &b_is_downstream_reconstructible);
+   fChain->SetBranchAddress("has_MCParticle_not_electron", &has_MCParticle_not_electron, &b_has_MCParticle_not_electron);
+   fChain->SetBranchAddress("is_downstream_reconstructible_not_electron", &is_downstream_reconstructible_not_electron, &b_is_downstream_reconstructible_not_electron);
+   fChain->SetBranchAddress("is_true_seed", &is_true_seed, &b_is_true_seed);
+   fChain->SetBranchAddress("seed_chi2PerDoF", &seed_chi2PerDoF, &b_seed_chi2PerDoF);
+   fChain->SetBranchAddress("seed_p", &seed_p, &b_seed_p);
+   fChain->SetBranchAddress("seed_pt", &seed_pt, &b_seed_pt);
+   fChain->SetBranchAddress("seed_nLHCbIDs", &seed_nLHCbIDs, &b_seed_nLHCbIDs);
+   fChain->SetBranchAddress("seed_nbIT", &seed_nbIT, &b_seed_nbIT);
+   fChain->SetBranchAddress("seed_nLayers", &seed_nLayers, &b_seed_nLayers);
+   fChain->SetBranchAddress("seed_x", &seed_x, &b_seed_x);
+   fChain->SetBranchAddress("seed_y", &seed_y, &b_seed_y);
+   fChain->SetBranchAddress("seed_tx", &seed_tx, &b_seed_tx);
+   fChain->SetBranchAddress("seed_ty", &seed_ty, &b_seed_ty);
+   fChain->SetBranchAddress("seed_mva_value", &seed_mva_value, &b_seed_mva_value);
+   Notify();
+}
+
+Bool_t class1::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void class1::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t class1::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef class1_cxx
---
+++ write_to_file.cpp
@@ -0,0 +1,63 @@
+#define class1_cxx
+#define class2_cxx
+#define class3_cxx
+#include "class1.h"
+#include "class2.h"
+#include "class3.h"
+
+using namespace std;
+
+void write_to_file(){
+	
+	cout<<"Hello World!"<<endl;
+	
+	TFile* f1 = new TFile("Brunel_BdJPsiKs_MagU_30k.root");
+
+    TDirectoryFile* dir = (TDirectoryFile*)f1->Get("ToolSvc.PatDebugTTTruthTool");
+    TTree* tree1 = (TTree*)dir->Get("DownstreamSeedDebugTuple");
+    TTree* tree2 = (TTree*)dir->Get("DownstreamTrackDebugTuple");
+    TTree* tree3 = (TTree*)dir->Get("chi2Tuple");
+
+    class1 c1(tree1);
+    class2 c2(tree2);
+    class3 c3(tree3);
+
+    
+    fstream f2;
+	fstream f3;
+	fstream f4;	
+    
+
+
+    f2.open("dsdt.csv", ios::out|ios::trunc);
+    f3.open("dtdt.csv",ios::out|ios::trunc);
+    f4.open("chi2.csv",ios::out|ios::trunc );
+    
+    for(int i =0; i<1000; i++){
+    	tree1->GetEntry(i);
+    	tree2->GetEntry(i);
+    	tree3->GetEntry(i);
+    	
+    	f2<<c1.has_MCParticle<<","<<c1.is_downstream_reconstructible<<","<<c1.has_MCParticle_not_electron<<","
+		<<c1.is_downstream_reconstructible_not_electron<<","<<c1.is_true_seed<<","<<c1.seed_chi2PerDoF<<","<<c1.seed_mva_value<<","<<c1.seed_nbIT<<","<<c1.seed_nLayers
+		<<","<<c1.seed_nLHCbIDs<<","<<c1.seed_p<<","<<c1.seed_pt<<","<<c1.seed_tx<<","<<c1.seed_ty<<","<<c1.seed_x<<","<<c1.seed_y<<endl;
+		
+		f3<<c2.hasT<<","<<c2.hasTT<<","<<c2.hasVelo<<","<<c2.isMomentumOver5GeV<<","<<c2.is_true_seed<<","<<c2.is_true_track<<","<<c2.seed_chi2PerDoF<<","<<c2.seed_nbIT
+		<<","<<c2.seed_nLHCbIDs<<","<<c2.seed_p<<","<<c2.seed_pt<<","<<c2.seed_tx<<","<<c2.seed_ty<<","<<c2.seed_x<<","<<c2.seed_y<<","<<c2.seed_z<<","<<c2.track_chi2
+		<<","<<c2.track_displX<<","<<c2.track_displY<<","<<c2.track_errXMag<<","<<c2.track_errYMag<<","<<c2.track_isIgnored<<","<<c2.track_mvaValue<<","<<c2.track_nbHit
+		<<","<<c2.track_p<<","<<c2.track_tx<<","<<c2.track_ty<<","<<c2.track_x<<","<<c2.track_y<<endl;
+		
+		f4<<c3.p<<","<<c3.nHits<<","<<c3.chi2<<","<<c3.AcceptCandidate<<","<<c3.AddOverlapRegions<<","<<c3.AddUHits
+		<<","<<c3.AddVHits<<","<<c3.BeampipeCut<<","<<c3.FitAndRemove<<","<<c3.Preselection<<","
+		<<c3.ReconstructibleAsDown<<","<<c3.ReconstructibleAsLong<<","<<c3.addUHits<<","<<c3.fisher<<","
+		<<c3.fitXProjection<<","<<c3.fitXprojection<<","<<c3.initEvent<<","<<c3.seedClassifier<<endl;
+		
+		
+    		
+	}
+    f2.close();
+    f3.close();
+    f4.close();
+		
+	
+}
