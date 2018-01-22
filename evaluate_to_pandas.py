43ba32c6fe4f847726024547b681cd3c9650608c
Zuzanna Zak <5zak@taurus.fis.agh.edu.pl>
2018-01-22 15:05:34 +0100

    added normalization and standardization

--- evaluate_to_pandas.py
+++ evaluate_to_pandas.py
@@ -1,9 +1,30 @@
 import pandas as pd
-
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
+import math
 
 def read_data(filepath='dsdt.csv'):
-    cols1 = ['has_MCParticle', 'is_downstream_reconstructible', 'has_MCParticle_not_electron', 'is_downstream_reconstructible_not_electron', 'is_true_seed', 'seed_chi2PerDoF', 'seed_mva_value', 'seed_nbIT', 'seed_nLayers', 'seed_nLHCbIDs', 'seed_p', 'seed_pt', 'seed_tx', 'seed_ty', 'seed_x', 'seed_y']
+    cols1 = ['seed_chi2PerDoF', 'seed_nbIT', 'seed_nLayers', 'seed_nLHCbIDs', 'seed_p', 'seed_pt', 'seed_tx', 'seed_ty', 'seed_x', 'seed_y', 'is_downstream_reconstructible']
     result = pd.read_csv(filepath, names=cols1, index_col=False)
-    result = result[['seed_chi2PerDoF', 'seed_p', 'seed_pt', 'seed_nLHCbIDs' , 'seed_nbIT'  , 'seed_nLayers', 'seed_x' , 'seed_y' , 'seed_tx', 'seed_ty', 'is_downstream_reconstructible']]
+    #print(result)
     return result
+
+
+
+def normalize_data(dat):
+    x = dat.values
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    dat_scaled = pd.DataFrame(x_scaled)
+    return dat_scaled
+
+def standardize_data(dat):
+    x = dat.values
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
+    standarized = pd.DataFrame(x)
+    return standarized
+
+
+
 
